50989f7d0694a6813cbb79bdbd5a5ad551d0f56b
beran <radek.beran@etnetera.cz>
2017-04-04 18:52:54 +0200

    Simple builder of base of MySQL create table command

---
+++ src/main/java/cz/etn/overview/mapper/MySqlSchemaBuilder.java
@@ -0,0 +1,88 @@
+package cz.etn.overview.mapper;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Optional;
+
+/**
+ * Constructs base of SQL DDL commands based on entity attributes.
+ * @author Radek Beran
+ */
+public class MySqlSchemaBuilder {
+
+    private static final String NEW_LINE = System.getProperty("line.separator");
+    private static final String UNKNOWN = "UNKNOWN";
+
+    public <E> String composeCreateTableSQL(String tableName, Attribute<E, ?>[] attributes) {
+        StringBuilder sb = new StringBuilder();
+        if (attributes != null && attributes.length > 0) {
+            Optional<String> primaryAttrNameOpt = getPrimaryAttributeName(attributes);
+            sb.append("CREATE TABLE IF NOT EXISTS `" + tableName + "` (" + NEW_LINE);
+            boolean first = true;
+            for (Attribute<E, ?> attr : attributes) {
+                if (first) {
+                    first = false;
+                } else {
+                    sb.append("," + NEW_LINE);
+                }
+                String sqlTypeName = getSqlTypeName(attr);
+                sb.append("\t`" + attr.getName() + "` " + sqlTypeName);
+                if (attr.isPrimary()) {
+                    sb.append(" NOT NULL");
+                    if (isNumber(attr)) {
+                        sb.append(" AUTO_INCREMENT");
+                    }
+                }
+            }
+            primaryAttrNameOpt.map (primaryAttrName -> {
+                sb.append("," + NEW_LINE);
+                sb.append("\tPRIMARY KEY (`" + primaryAttrName + "`)");
+                return null;
+            });
+            sb.append(NEW_LINE + ");" + NEW_LINE);
+        }
+        return sb.toString();
+    }
+
+    protected Optional<String> getPrimaryAttributeName(Attribute<?, ?>[] attributes) {
+        Optional<String> name = Optional.empty();
+        if (attributes != null) {
+            for (Attribute<?, ?> attr : attributes) {
+                if (attr.isPrimary()) {
+                    name = Optional.ofNullable(attr.getName());
+                    break;
+                }
+            }
+        }
+        return name;
+    }
+
+    protected <E, A> String getSqlTypeName(Attribute<E, A> attribute) {
+        String typeName = UNKNOWN;
+        if (attribute instanceof Attr) {
+            Attr attr = (Attr)attribute;
+            Class<A> attrClass = attr.getAttributeClass();
+            if (String.class.isAssignableFrom(attrClass)) {
+                typeName = "VARCHAR(" + UNKNOWN + ")";
+            } else if (Byte.class.isAssignableFrom(attrClass) || Integer.class.isAssignableFrom(attrClass) || Long.class.isAssignableFrom(attrClass)) {
+                typeName = "INT(" + UNKNOWN + ")";
+            } else if (Instant.class.isAssignableFrom(attrClass) || Date.class.isAssignableFrom(attrClass)) {
+                typeName = "DATETIME";
+            } else if (BigDecimal.class.isAssignableFrom(attrClass)) {
+                typeName = "DECIMAL(" + UNKNOWN + ", " + UNKNOWN + ")";
+            }
+        }
+        return typeName;
+    }
+
+    protected <E, A> boolean isNumber(Attribute<E, A> attribute) {
+        boolean number = false;
+        if (attribute instanceof Attr) {
+            Attr attr = (Attr)attribute;
+            Class<A> attrClass = attr.getAttributeClass();
+            number = Byte.class.isAssignableFrom(attrClass) || Integer.class.isAssignableFrom(attrClass) || Long.class.isAssignableFrom(attrClass) || BigDecimal.class.isAssignableFrom(attrClass);
+        }
+        return number;
+    }
+}
